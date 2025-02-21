ebab98536df48ea0bff7906b9ab7b21b7412e171
emanuel-maltese <emanuel.maltese@galileo.galileicrema.it>
2025-02-21 15:49:19 +0100

    prova21_02_25

--- es1.py
+++ es1.py
@@ -4,7 +4,11 @@ c = int(input("Inserisci il terzo numero: "))
 
 if a >= b and a >= c:
     print(a)
+    print("e' il numero piu' grande")
 elif b >= a and b >= c:
     print(b)
+    print("e' il numero piu' grande")
 elif c >= a and c >= b:
     print(c)
+    print("e' il numero piu' grande")
+
